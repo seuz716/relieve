19f6910608f8a93e5919ef6a5fbec22da2fbf656
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2022-02-27 21:11:10 -0500

    make it better

--- src/components/customerInversion/CustomerInversion.jsx
+++ src/components/customerInversion/CustomerInversion.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import './CustomerInversion.css'
 import { Link } from 'react-router-dom'
+import familia from '../../images/cocina2.png'
+import finca from '../../images/vistaVentana.png'
+import farol from '../../images/sala2.png'
+import panoramica from '../../images/panoramica.png'
+import edificio from '../../images/edificio.png'
+import edificio1 from '../../images/panoramica1.png'
 
 export default function CustomerInversion() {
     return (
         <div className="CustomerInversion">
               <div className="ContenedorBtn">
-                <img className="Customer" src="https://cdn.pixabay.com/photo/2019/10/02/14/23/house-keys-4521073_960_720.jpg" alt="" width="100%" /> <br />
+                <img className="Customer" src={edificio} alt="" width="100%" /> <br />
                 <Link class="btnInversion" to="#">CONOCE MÁS</Link>
               </div> 
               
               <div className="ContenedorBtn">
-                <img className="Customer" src="https://blog.marivalresorts.com/media/uploads/2016/07/blog_Fe.jpg" alt="" width="100%" /> <br /> 
+                <img className="Customer" src={edificio1} alt="" width="100%" /> <br /> 
                 <Link class="btnInversion" to="#">CONOCE MÁS</Link>
               </div>  
             
