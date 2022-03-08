48e65fa3633838699c4365c34240ee332281d172
CESAR ANDRES ABADIA DAVILA <80592969+seuz716@users.noreply.github.com>
2022-03-08 13:58:40 -0500

    about done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,10 @@ export default function About() {
     return (
         <div className="contenedorAbout" >
             <h1 className="Qsomos">QUÍENES SOMOS</h1>
-            <p className="PqSomos">Occaecat irure qui elit ad fugiat aute tempor nisi fugiat cillum. Proident consectetur consectetur qui mollit tempor deserunt labore est tempor. Qui est laborum cupidatat aute elit sunt culpa dolore consequat elit. Cupidatat pariatur qui do occaecat do dolor esse eiusmod consectetur est ea dolor.Aliqua proident irure velit irure proident. Commodo pariatur aute occaecat laboris duis culpa enim cillum ipsum aliqua officia dolor veniam. Cupidatat elit veniam consequat tempor labore ex consequat occaecat. Sit ut do eiusmod anim proident non eiusmod nostrud nostrud ex officia est consectetur do. Ad duis id pariatur cupidatat mollit cupidatat adipisicing aliqua officia. Duis ut eiusmod nulla ea in veniam fugiat magna aute ut laborum ad nisi labore. Est et labore voluptate cupidatat reprehenderit ipsum nulla est dolore ex proident cillum.</p>
+            <p className="PqSomos">Somos una empresa que asumimos cada proyecto con responsabilidad y seriedad, para nosotros es
+                de suma importancia construir con sentido social y ambiental, nuestra labor principal es brindar las
+                mejores opciones teniendo en cuenta las necesidades y requerimientos propios, de cada cliente, con
+                transparencia y plena satisfacción de haber superado las expectativas..</p>
             <Link className="botonAbout" to="MisionVision">CONOCE MÁS</Link> 
         </div>
     )
