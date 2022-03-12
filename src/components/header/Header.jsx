f0e9b1bba31b81ad4483260695e634cd42cef01d
CESAR ANDRES ABADIA DAVILA <80592969+seuz716@users.noreply.github.com>
2022-03-12 11:35:22 -0500

    'tipografia'

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,40 +4,36 @@ import logo from '../../images/logo.jpg'
 import './header.css' 
 
 export default function Header() {
-    return (
-        <div>
-<<<<<<< HEAD
-            <nav className="navbar navbar-expand-lg navbar-light fw-bold" style={{background:'#ffffff'}}>
-                <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img className='foto' src={logo}  alt="Relieve logo"/><strong>   Relieve SAS</strong></Link>
-=======
-            <nav className="navbar navbar-expand-lg navbar-light fw-bold" style={{background:'#ffffff '}}>
-                <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img src={logo} width="90" height="60" alt="Relieve logo"/><strong> Relieve Ingenieria SAS</strong></Link>
->>>>>>> 3e18c8b90fabbdccb0f683db4d5ceb40975fb8a6
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                      <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="login">Login</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="register">Registro</Link>
-                    </li>
+  return (
+      <div>
 
-                  </ul>
-{/*                   <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                  </form> */}
-                </div>
+          <nav className="navbar navbar-expand-lg navbar-light fw-bold" style={{background:'#86CFEE'}}>
+              <div className="container-fluid">
+              <Link className="navbar-brand" to="/"><img src={logo} width="90" height="60" alt="Relieve logo"/><strong> Relieve Ingenieria SAS</strong></Link>
+
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="login">Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="register">Registro</Link>
+                  </li>
+
+                </ul>
+                 <form className="d-flex">
+                  <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                  <button className="btn btn-outline-success" type="submit">Search</button>
+                </form> 
               </div>
-            </nav>
-        </div>
-    )
+            </div>
+          </nav>
+      </div>
+  )
 }
